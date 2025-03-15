7fd5f5ed74432faf19908fe899ba33f19921a91b
Kenneth Lauer <kenneth.j.lauer@protonmail.com>
2025-03-15 16:22:44 -0400

    Denoted the function _process() argument delta as unused with an "_" to quell a compiler warning.

--- player.gd
+++ player.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 @export var speed = 0.15
 var target_velocity = Vector3.ZERO
 
-func _process(delta):
+func _process(_delta):
 	var direction = Vector3.ZERO
 	
 	if Input.is_action_pressed("move_right"):
