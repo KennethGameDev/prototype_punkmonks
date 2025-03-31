2cd9e22ff394df1fa82967b15387377b5533c69c
Kenneth Lauer <kenneth.j.lauer@protonmail.com>
2025-03-31 14:15:00 -0400

    I forgot to stage the file :) And I think I finally discovered how to separate the commit title and description with just the godot plugin. I will find out with this commit. Cleaned up and improved comments for the get_movement_direction() function. Removed uneccessary function check_simultaneous_input() function since it became obsolete several commits ago.

--- scripts/game_input_events.gd
+++ scripts/game_input_events.gd
@@ -1,34 +1,52 @@
 class_name GameInputEvents
 
 static var direction: Vector2
+
+## The 'primary movement axis' determines which movement axis (horizontal or
+## vertical) the player must be moving in. It is set when the player presses any
+## movement input and it is later used when handling the case where two
+## opposing digital inputs are being held. Works in a top-down, 2D game that
+## restricts movement to up, down, left, and right
 # No Moving: -1
 # Horizontal Movement: 0
 # Vertical Movement: 1
 static var primary_movement_axis: int = -1
 
 static func get_movement_direction() -> Vector2:	
-	# Get input direction (includes diagonal movement)
+	## Get input direction (includes diagonal movement)
 	direction = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	
+	## Filter out diagonals
 	# The player is trying to move horizontally
 	if Input.is_action_pressed("move_right") || Input.is_action_pressed("move_left"):
-		# Check if they're already moving along an axis
+		# Check if they're starting from a standstill
 		if primary_movement_axis == -1:
-			# If no, set the primary axis to 0 for horizontal movement
+			# If yes, set the primary axis to 0 for horizontal movement
 			primary_movement_axis = 0
+		# If they're already moving, set direction's vertical component to 0
+		# (It doesn't matter what axis we check here since pressing the other
+		# horizontal input would make the player stop moving, which is natural)
 		else:
 			direction.y = 0
+	# The player is trying to move vertically
 	elif Input.is_action_pressed("move_forward") || Input.is_action_pressed("move_back"):
-		# Check if they're already moving along an axis
+		# Check if they're starting from a standstill
 		if primary_movement_axis == -1:
-			# If no, set the primary axis to 1 for vertical movement
+			# If yes, set the primary axis to 1 for vertical movement
 			primary_movement_axis = 1
+		# If they're already moving, set direction's horizontal component to 0
+		# (It doesn't matter what axis we check here since pressing the other
+		# vertical input would make the player stop moving, which is natural)
 		else:
 			direction.x = 0
+	# The player is standing still (not pressing any direction)
 	else:
+		# Primary axis becomes -1 for not moving and direction is zeroed out
 		primary_movement_axis = -1
 		direction = Vector2.ZERO
 	
+	## Handle simultaneous digital input direction part 1
+	## Horizontal -> Vertical
 	# If the primary axis is currently horizontal,
 	# check for simultaneous movement on the vertical axis
 	if primary_movement_axis == 0:
@@ -45,6 +63,8 @@ static func get_movement_direction() -> Vector2:
 			if Input.is_action_just_released("move_right") || Input.is_action_just_released("move_left"):
 				primary_movement_axis = 1
 	
+	## Handle simultaneous digital input direction part 2
+	## Vertical -> Horizontal
 	# If the primary axis is currently vertical,
 	# check for simultaneous movement on the horizontal axis
 	if primary_movement_axis == 1:
@@ -61,6 +81,7 @@ static func get_movement_direction() -> Vector2:
 			if Input.is_action_just_released("move_forward") || Input.is_action_just_released("move_back"):
 				primary_movement_axis = 0
 	
+	# Normalize the direction vector (set the vector's length to 1)
 	direction = direction.normalized()
 	
 	return direction
@@ -71,16 +92,6 @@ static func is_movement_input() -> bool:
 		return false
 	else:
 		return true
-
-
-static func check_simulatneous_movement() -> String:
-	var result: String
-	
-	if direction == Vector2.ZERO:
-		result = "null"
-		
-	
-	return result
 
 
 static func get_primary_movement_axis() -> String:
