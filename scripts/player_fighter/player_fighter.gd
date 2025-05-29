cd31a0cb2e7a5ad5b2204324451b4d5aa2499ca4
Kenneth Lauer <kenneth.j.lauer@protonmail.com>
2025-05-29 18:45:46 -0400

    State Machines... Again
    Second attempt at a state machine. Making it myself this time with the help of a youtube tutorial as a guide. Fingers crossed.

---
+++ scripts/player_fighter/player_fighter.gd
@@ -0,0 +1,14 @@
+class_name PlayerFighter
+extends CharacterBody2D
+
+@onready var state_machine: StateMachine = $"State Machine"
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _ready(): state_machine.init()
+
+func _process(delta): state_machine.process_frame(delta)
+
+func _physics_process(delta): state_machine.process_physics(delta)
+
+func _input(event): state_machine.process_input(event)
